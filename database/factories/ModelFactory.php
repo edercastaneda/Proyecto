6d0570350136cabac2c3b1ba1109a7b566fc80a4
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-09-18 16:11:04 -0600

    Definido model factory para alumnos

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,14 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(\App\Models\Alumno::class, function (Faker\Generator $faker) {
+    return [
+        'nombre' => $faker->name,
+        'apellido' => $faker->name,
+        'direccion' => '',
+        'telefono' => $faker->numberBetween(45589820,55589820),
+        'responsable' => $faker->name
+    ];
+});
