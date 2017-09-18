ce44668120b84b2e4aeb0e2113df9f330d1b616b
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-09-18 17:34:19 -0600

    Mostrar imagen en vista show andamio alumnos

--- resources/views/alumnos/show.blade.php
+++ resources/views/alumnos/show.blade.php
@@ -9,9 +9,26 @@
     <div class="content">
         <div class="box box-primary">
             <div class="box-body">
-                <div class="row" style="padding-left: 20px">
-                    @include('alumnos.show_fields')
-                    <a href="{!! route('alumnos.index') !!}" class="btn btn-default">Back</a>
+                <div class="row" >
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+
+                        @include('alumnos.show_fields')
+                    </div>
+                    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                        @if($alumno->alumnoImages->count()>0)
+                            @foreach($alumno->alumnoImages as $key => $image)
+                                <div class="form-group col-sm-3">
+                                    <img src="{{srcImgBynary($image)}}" alt="{{$image->nombre}}" class="img-responsive">
+                                </div>
+                            @endforeach
+                        @else
+                            <h3 class="text-warning text-center">No hay imagenes cargadas</h3>
+                        @endif
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+
+                        <a href="{!! route('alumnos.index') !!}" class="btn btn-default">Back</a>
+                    </div>
                 </div>
             </div>
         </div>
