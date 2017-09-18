88bcb9f1914865341d0ef3dbad1f3aeca31e147b
Esdras Altamirano <altamiranoesdras@gmail.com>
2017-09-18 17:02:06 -0600

    Fix menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -14,12 +14,3 @@
 <li class="{{ Request::is('alumnos*') ? 'active' : '' }}">
     <a href="{!! route('alumnos.index') !!}"><i class="fa fa-edit"></i><span>Alumnos</span></a>
 </li>
-
-<li class="{{ Request::is('alumnos*') ? 'active' : '' }}">
-    <a href="{!! route('alumnos.index') !!}"><i class="fa fa-edit"></i><span>Alumnos</span></a>
-</li>
-
-<li class="{{ Request::is('alumnos*') ? 'active' : '' }}">
-    <a href="{!! route('alumnos.index') !!}"><i class="fa fa-edit"></i><span>Alumnos</span></a>
-</li>
-
